2691dc5b7d75b052a3a9270384267af960fa964c
JackC <jackc0022@gmail.com>
2019-05-20 19:13:01 -0700

    Created boundary and ball. Ball will fire on click in direction of mouse.

---
+++ BrickBreakerGame/Assets/Scripts/Ball_Controller.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball_Controller : MonoBehaviour
+{
+    public bool onGround, lineDrawn;
+    public int speed;
+
+
+    void Start() {
+        speed = 3;
+        onGround = true;
+        transform.position = new Vector2 (0, -3.783198f);
+    }
+
+    void Update() {
+        if (onGround)
+            transform.LookAt(Input.mousePosition);
+        if (!onGround)
+            transform.position += transform.forward * Time.deltaTime * speed;
+        if (Input.GetMouseButtonUp(0))
+            onGround = false;
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.tag == "Ground") {
+            onGround = true;
+            GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        }
+        else {
+            //bounce off
+        }
+    }
+}
