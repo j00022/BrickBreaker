8b2f862967f05e7fbdd18855af7d6966d92b93f4
JackC <jackc0022@gmail.com>
2019-05-21 21:52:57 -0700

    Ball now correctly aims at mouse. Will not aim less than 10 deg and not more than 170 deg.

--- BrickBreakerGame/Assets/Scripts/Ball_Controller.cs
+++ BrickBreakerGame/Assets/Scripts/Ball_Controller.cs
@@ -6,19 +6,34 @@ public class Ball_Controller : MonoBehaviour
 {
     public bool onGround, lineDrawn;
     public int speed;
+    public float angle;
+    Rigidbody2D rb;
+    Vector3 mousePos, objectPos;
 
 
     void Start() {
         speed = 3;
         onGround = true;
-        transform.position = new Vector2 (0, -3.783198f);
+        transform.position = new Vector2 (0, -3.783198f); //On ground
+        rb = GetComponent<Rigidbody2D>();
     }
 
     void Update() {
-        if (onGround)
-            transform.LookAt(Input.mousePosition);
+        if (onGround) {
+            mousePos = Input.mousePosition;
+            objectPos = Camera.main.WorldToScreenPoint(transform.position);
+            mousePos.x -= objectPos.x;
+            mousePos.y -= objectPos.y;
+
+            angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+            if (angle < 10 && angle > -90)
+                angle = 10;
+            if (angle > 170 || angle < -90)
+                angle = 170;
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+        }
         if (!onGround)
-            transform.position += transform.forward * Time.deltaTime * speed;
+            rb.velocity = transform.right * speed;
         if (Input.GetMouseButtonUp(0))
             onGround = false;
     }
