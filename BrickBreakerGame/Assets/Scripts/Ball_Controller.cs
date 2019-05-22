5b0f57bc4eecd4b43800610437cb7568c60551d8
JackC <jackc0022@gmail.com>
2019-05-22 01:57:49 -0700

    Added trajectory line that calculates one bounce and some distance after

--- BrickBreakerGame/Assets/Scripts/Ball_Controller.cs
+++ BrickBreakerGame/Assets/Scripts/Ball_Controller.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class Ball_Controller : MonoBehaviour
 {
-    public bool onGround, lineDrawn;
+    public bool onGround, drawLine;
     public int speed;
     public float angle;
     Rigidbody2D rb;
+    LineRenderer line;
     Vector3 mousePos, objectPos;
+    RaycastHit2D hit;
 
 
     void Start() {
@@ -16,6 +18,9 @@ public class Ball_Controller : MonoBehaviour
         onGround = true;
         transform.position = new Vector2 (0, -3.783198f); //On ground
         rb = GetComponent<Rigidbody2D>();
+        line = gameObject.AddComponent<LineRenderer>();
+        line.widthMultiplier = 0.03f;
+        line.positionCount = 3;
     }
 
     void Update() {
@@ -31,16 +36,21 @@ public class Ball_Controller : MonoBehaviour
             if (angle > 170 || angle < -90)
                 angle = 170;
             transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+            DrawRay();
         }
         if (!onGround)
             rb.velocity = transform.right * speed;
-        if (Input.GetMouseButtonUp(0))
+        if (Input.GetMouseButtonUp(0)) {
             onGround = false;
+            drawLine = false;
+            DrawRay();
+        }
     }
 
     public void OnCollisionEnter2D(Collision2D collision) {
         if (collision.gameObject.tag == "Ground") {
             onGround = true;
+            drawLine = true;
             GetComponent<Rigidbody2D>().velocity = Vector2.zero;
         }
         else {
@@ -48,7 +58,15 @@ public class Ball_Controller : MonoBehaviour
         }
     }
 
-    public void OnDrawGizmos() {
-        
+    public void DrawRay() {
+        hit = Physics2D.CircleCast(transform.position, 0.161f, transform.right);
+        line.SetPosition(0, transform.position);
+        line.SetPosition(1, hit.point);
+        Vector3 rayReflect = Vector3.Reflect((Vector3)hit.centroid - line.GetPosition(0), hit.normal);
+        hit = Physics2D.Raycast(hit.centroid, rayReflect.normalized, 4f);
+        if ((Vector3)hit.point == Vector3.zero)
+            line.SetPosition(2, line.GetPosition(1) + rayReflect.normalized * 4f);
+        else
+            line.SetPosition(2, line.GetPosition(1) + rayReflect.normalized * hit.distance);
     }
 }
